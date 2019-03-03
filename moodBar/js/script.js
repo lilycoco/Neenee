22e4b9d90479b65a177de3b4806616b9f24d567a
lilycoco <ry.sep27@gmail.com>
2019-03-03 18:55:05 +0900

    nouislider

--- moodBar/js/script.js
+++ moodBar/js/script.js
@@ -1,33 +1,31 @@
-function hexFromRGB(r, g, b) {
-  var hex = [
-    r.toString( 16 ),
-    g.toString( 16 ),
-    b.toString( 16 )
-  ];
-  $.each( hex, function( nr, val ) {
-    if ( val.length === 1 ) {
-      hex[ nr ] = "0" + val;
-    }
+$(function () {
+  var resultElement = document.getElementById('result');
+  var sliders = document.getElementsByClassName('sliders');
+  var colors = [0, 0, 0];
+  
+  [].slice.call(sliders).forEach(function (slider, index) {
+  
+    noUiSlider.create(slider, {
+      start: 127,
+      connect: [true, false],
+      orientation: "vertical",
+      range: {
+        'min': 0,
+        'max': 255
+      },
+      format: wNumb({
+        decimals: 0
+      })
+    });
+
+    // Bind the color changing function to the update event.
+    slider.noUiSlider.on('update', function () {
+      colors[index] = slider.noUiSlider.get();
+
+      var color = 'rgb(' + colors.join(',') + ')';
+
+      resultElement.style.background = color;
+      resultElement.style.color = color;
+    });
   });
-  return hex.join( "" ).toUpperCase();
-}
-function refreshSwatch() {
-  var red = $( "#red" ).slider( "value" ),
-    green = $( "#green" ).slider( "value" ),
-    blue = $( "#blue" ).slider( "value" ),
-    hex = hexFromRGB( red, green, blue );
-  $( "#swatch" ).css( "background-color", "#" + hex );
-}
-$(function() {
-  $( "#red, #green, #blue" ).slider({
-    orientation: "horizontal",
-    range: "min",
-    max: 255,
-    value: 127,
-    slide: refreshSwatch,
-    change: refreshSwatch
-  });
-  $( "#red" ).slider( "value", 255 );
-  $( "#green" ).slider( "value", 140 );
-  $( "#blue" ).slider( "value", 60 );
 });
